6402b22769e5a9c0044df5613b5c4265ab4d8977
辜思学 <gusixue@ipaychat.com>
2023-03-30 00:58:49 +0800

    feat:Binary Search Template

--- src/main/java/template/BinarySearch.java
+++ src/main/java/template/BinarySearch.java
@@ -1,33 +1,71 @@
 package template;
 
 /**
- * todo:二分搜索模板
+ * @description 二分搜索模板
  * @author gusixue
  * @date 2023/3/26
  */
 public class BinarySearch {
 
     /**
-     * nums 数组中小于等于 query 的最大下标
-     * 如果均大于 query 返回 0
+     * 二分搜索：返回非递减数组中小于等于 key 的最大下标
+     * 如果均大于 key 返回 -1
      */
-    public int binarySearchMin(long[] nums, int key) {
+    public int binarySearchHigh(long[] nums, int key) {
         int left = 0;
         int right = nums.length - 1;
+        int res = -1;
 
-        while (left < right) {
-            // 避免 left + right 越界
-            int mid = ((right + 1 - left) >> 1) + left;
-
-            System.out.println(left + ":" + right + ":" + mid + ":" + key);
+        while (left <= right) {
+            // 避免 left+right 越界
+            int mid = ((right - left) >> 1) + left;
 
             if (nums[mid] > key) {
                 right = mid - 1;
+
+            } else if (nums[mid] < key) {
+                res = mid;
+                left = mid + 1;
+
             } else {
-                left = mid;
+                res = mid;
+                left = mid + 1;
+
             }
         }
-        return left;
+
+        return res;
+    }
+
+
+    /**
+     * 二分搜索：返回非递减数组中大于等于 key 的最小下标
+     * 如果均小于 key 返回 -1
+     */
+    public int binarySearchLow(long[] nums, int key) {
+        int left = 0;
+        int right = nums.length - 1;
+        int res = -1;
+
+        while (left <= right) {
+            // 避免 left+right 越界
+            int mid = ((right - left) >> 1) + left;
+
+            if (nums[mid] > key) {
+                res = mid;
+                right = mid - 1;
+
+            } else if (nums[mid] < key) {
+                left = mid + 1;
+
+            } else {
+                res = mid;
+                right = mid - 1;
+
+            }
+        }
+
+        return res;
     }
 
 }
--- src/test/java/template/BinarySearchTest.java
+++ src/test/java/template/BinarySearchTest.java
@@ -13,7 +13,35 @@ public class BinarySearchTest {
     public void test() {
         BinarySearch binarySearch = new BinarySearch();
 
-        int res = binarySearch.binarySearchMin(new long[]{1, 3, 5, 8}, 7);
+        int res = binarySearch.binarySearchLow(new long[]{1, 1, 1, 2, 2, 3, 3}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchLow(new long[]{2, 2, 3, 3}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchLow(new long[]{1}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchLow(new long[]{1, 1}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchLow(new long[]{1, 1, 3, 3}, 2);
+        System.out.println(res);
+
+
+        res = binarySearch.binarySearchHigh(new long[]{1, 1, 2, 2, 3, 3, 3}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchHigh(new long[]{1, 1, 2, 2, 2}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchHigh(new long[]{3}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchHigh(new long[]{3, 3}, 2);
+        System.out.println(res);
+
+        res = binarySearch.binarySearchHigh(new long[]{1, 1, 3, 3}, 2);
         System.out.println(res);
 
     }
