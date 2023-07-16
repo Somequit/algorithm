96afc6746c7d22ef3cad6b221329f0d7fcdc04ec
辜思学 <gusixue@ipaychat.com>
2023-07-16 14:24:58 +0800

    feat：Leetcode Contest 354 update 4

--- src/main/java/leetcode/contest/contest_354/Contest4.java
+++ src/main/java/leetcode/contest/contest_354/Contest4.java
@@ -15,39 +15,31 @@ public class Contest4 {
     public static void main(String[] args) {
         Contest4 contest = new Contest4();
 
-        String word = AlgorithmUtils.systemInString();
-        String forbiddenJson = AlgorithmUtils.systemInString();
+        while (true) {
+            String word = AlgorithmUtils.systemInString();
+            List<String> forbidden = CollectionUtils.arrayToList(AlgorithmUtils.systemInArrayString());
 
-        int res = contest.solution(word, JSONObject.parseArray(forbiddenJson, String.class));
-        System.out.println(res);
-
-//        while (true) {
-//            String word = AlgorithmUtils.systemInString();
-//            List<String> forbidden = CollectionUtils.arrayToList(AlgorithmUtils.systemInArrayString());
-//
-//            int res = contest.solution(word, forbidden);
-//            System.out.println(res);
-//        }
+            int res = contest.solution(word, forbidden);
+            System.out.println(res);
+        }
 
     }
 
     class TreeNode{
         Map<Character,TreeNode> map;
+        // 使用 map.get.isEnd 判断是否结尾
         boolean isEnd;
         public TreeNode(){
-            map = new HashMap<>(32);
+            map = new HashMap<>();
         }
-//
-//        @Override
-//        public String toString() {
-//            return "isEnd=" + isEnd + " TreeNode{"
-//                    + " map=" + map +
-//                    +'}';
-//        }
     }
 
     TreeNode root;
 
+    /**
+     * 将 word 存入字典树
+     * @param word
+     */
     public void insert(String word) {
         TreeNode node = root;
         for(int i = 0;i < word.length();++i){
@@ -60,94 +52,77 @@ public class Contest4 {
         node.isEnd = true;
     }
 
+    /**
+     * 返回 word 匹配到 Trie 树的最短长度
+     */
     public int searchMinNum(String word) {
         TreeNode node = root;
         for(int i = 0;i < word.length();++i){
             char index = word.charAt(i);
-            // System.out.println(node);
             if(!node.map.containsKey(index)){
                 return -1;
             }
-            if (node.map.get(index).isEnd) {
+            node = node.map.get(index);
+
+            // 中途存在直接返回长度（node 匹配 index 后 next 过了）
+            if (node.isEnd) {
                 return i + 1;
             }
-            node = node.map.get(index);
         }
+        // node 匹配 index 后 next 过了
         return node.isEnd == true ? word.length() : -1;
-    }
-
-    public boolean startsWith(String prefix) {
-        TreeNode node = root;
-        for(int i = 0;i < prefix.length();++i){
-            char index = prefix.charAt(i);
-            if(!node.map.containsKey(index)){
-                return false;
-            }
-            node = node.map.get(index);
-        }
-        return true;
     }
 
     public int solution(String word, List<String> forbidden) {
         int res = 0;
 
-        System.out.println(System.currentTimeMillis());
+        // 初始化
         root = new TreeNode();
+
+        // 去重并加入 Trie 树
         Set<String> forbiddenSet = new HashSet<>(forbidden.size() << 1);
         for (String forbiddenStr : forbidden) {
-//            System.out.println(forbiddenStr);
+
             if (!forbiddenSet.contains(forbiddenStr)) {
                 forbiddenSet.add(forbiddenStr);
+                // 加入 Trie 树
                 insert(forbiddenStr);
             }
         }
-        System.out.println(System.currentTimeMillis());
 
 
-        int[] wordMinMath = new int[word.length()];
-
-        int maxWordMinMath = 0;
+        // word[i] 当前元素为前缀最短匹配的长度，无法匹配则返回 -1
+        int[] wordMinMatch = new int[word.length()];
         int len = word.length();
         for (int i = 0; i < len; i++) {
             String wordTemp = word.substring(i, Math.min(len, i + 10));
-            // System.out.println(wordTemp);
-            wordMinMath[i] = searchMinNum(wordTemp);
-            maxWordMinMath = Math.max(maxWordMinMath, wordMinMath[i]);
+            wordMinMatch[i] = searchMinNum(wordTemp);
 
         }
-        System.out.println(maxWordMinMath + " : " + len);
-        // System.out.println(Arrays.toString(wordMinMath));
-        System.out.println(System.currentTimeMillis());
 
-        int count = 0;
         int cur = 0;
         for (int i = 0; i < len; i++) {
 
-            if (wordMinMath[i] != -1) {
-                int curOrigin = cur;
-                cur += wordMinMath[i] - 1;
+            // 前缀能够匹配 forbidden[]
+            if (wordMinMatch[i] != -1) {
 
-                int curTemp = 0;
-                for (int j = i + 1; j < len && j < i + 1 + wordMinMath[i]; j++) {
-                    count++;
-                    curTemp++;
-                    if (wordMinMath[j] != -1) {
-                        cur = Math.min(cur, curOrigin + curTemp + wordMinMath[j] - 1);
+                // 找到能匹配的最远距离（forbidden[] 最大为 10）
+                int curMaxMatch = wordMinMatch[i] - 1;
+                for (int j = i + 1; j < i + wordMinMatch[i]; j++) {
+                    // [i, i+wordMinMatch[i]] 中 j 元素前缀匹配到的 forbidden[] 可能更短
+                    if (wordMinMatch[j] != -1) {
+                        curMaxMatch = Math.min(curMaxMatch, (j - i) + (wordMinMatch[j] - 1));
                     }
                 }
-                // System.out.println(cur);
 
-                res = Math.max(res, cur);
+                res = Math.max(res, cur + curMaxMatch);
                 cur = 0;
 
             } else {
                 cur ++;
                 res = Math.max(res, cur);
-                // System.out.println(cur);
             }
         }
-
-        System.out.println(System.currentTimeMillis());
 
         return res;
     }
