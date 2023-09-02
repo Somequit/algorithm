5638b1a248519f82afe48ecf92debda17392386d
辜思学 <gusixue@ipaychat.com>
2023-09-02 23:30:28 +0800

    feat：Leetcode Double 112

---
+++ src/main/java/leetcode/contest/double_112/Contest2.java
@@ -0,0 +1,31 @@
+package leetcode.contest.double_112;
+
+import java.util.List;
+
+
+/**
+ * @author gusixue
+ * @date 2023/7/22
+ */
+public class Contest1 {
+
+    public static void main(String[] args) {
+        Contest1 contest = new Contest1();
+
+        while (true) {
+//            int purchaseAmount = AlgorithmUtils.systemInNumberInt();
+
+//            int res = contest.solution(purchaseAmount);
+//            System.out.println(res);
+        }
+
+    }
+
+    private int solution(List<Integer> nums, int target) {
+
+        return 0;
+    }
+
+
+
+}
---
+++ src/main/java/leetcode/contest/double_112/Contest3.java
@@ -0,0 +1,31 @@
+package leetcode.contest.double_112;
+
+import java.util.List;
+
+
+/**
+ * @author gusixue
+ * @date 2023/7/22
+ */
+public class Contest2 {
+
+    public static void main(String[] args) {
+        Contest2 contest = new Contest2();
+
+        while (true) {
+//            int purchaseAmount = AlgorithmUtils.systemInNumberInt();
+
+//            int res = contest.solution(purchaseAmount);
+//            System.out.println(res);
+        }
+
+    }
+
+    private int solution(List<Integer> nums, int target) {
+
+        return 0;
+    }
+
+
+
+}
