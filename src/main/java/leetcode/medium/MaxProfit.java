22d13fb7204552781827ea53cd790dd929864d2f
辜思学 <gusixue@ipaychat.com>
2023-09-20 01:57:31 +0800

    feat:Leetcode 287

--- src/main/java/leetcode/medium/MaxProfit.java
+++ src/main/java/leetcode/medium/MaxProfit.java
@@ -31,8 +31,9 @@ public class MaxProfit {
     /**
      * 动态规划dp：dp[i] 代表第 i 天卖出可以获得最大的收益，初始化 dp[0]=0
      * 转移方程：dp[i]=max(dp[i], dp[i-1], prices[i]-price[i-j]+dp[i-j-2]) 1<=j<=i，注意当 i-j-2 < 0 时直接不要
-     * 优化：为方便计算，现将 dp[i] 变成 dp[i+2] 整体右移，初始化 dp[0]=dp[1]=dp[2]=0，因为 prices[i] 不变，因此每次仅需要求 dp[i-j-2]-price[i-j] 的最大值即可
-     * n 为 prices.length，时间复杂度：O（n），空间复杂度：O（n）
+     * 时间优化：为方便计算，现将 dp[i] 变成 dp[i+2] 整体右移，初始化 dp[0]=dp[1]=dp[2]=0，因为 prices[i] 不变，因此每次仅需要求 dp[i-j-2]-price[i-j] 的最大值即可
+     * 空间优化：由于 dp[i+2] 仅仅与 dp[i+1]、dp[i-1] 有关，因此可以只使用 dp[4] 的滚动数组，每次对 4 取模即可
+     * n 为 prices.length，时间复杂度：O（n），空间复杂度：O（1）
      */
     private int solution(int[] prices) {
         // 特殊判断
@@ -41,7 +42,7 @@ public class MaxProfit {
         }
 
         // 动态规划
-        int[] dp = new int[prices.length + 2];
+        int[] dp = new int[4];
         dp[0] = 0;
         dp[1] = 0;
         dp[2] = 0;
@@ -49,11 +50,11 @@ public class MaxProfit {
         // 转移方程：dp[i+2]=max(dp[i+1], prices[i]-price[i-j]+dp[i-j]) 1<=j<=i，注意整体右移了 2
         int maxDpPrice = Integer.MIN_VALUE;
         for (int i = 1; i < prices.length; i++) {
-            maxDpPrice = Math.max(maxDpPrice, dp[i - 1] - prices[i - 1]);
-            dp[i + 2] = Math.max(dp[i + 1], prices[i] + maxDpPrice);
+            maxDpPrice = Math.max(maxDpPrice, dp[(i - 1 + 4) % 4] - prices[i - 1]);
+            dp[(i + 2) % 4] = Math.max(dp[(i + 1) % 4], prices[i] + maxDpPrice);
         }
 //        System.out.println(Arrays.toString(dp));
 
-        return dp[prices.length + 1];
+        return dp[(prices.length + 1) % 4];
     }
 }
