12970f910f393da9e82ca833752331416aceb3f1
辜思学 <gusixue@ipaychat.com>
2023-10-29 05:14:12 +0800

    Leetcode Contest Template

---
+++ src/main/java/leetcode/contest/Contest1.java
@@ -0,0 +1,45 @@
+package leetcode.contest;
+
+
+import javafx.util.Pair;
+import utils.AlgorithmUtils;
+
+import java.util.*;
+
+/**
+ */
+public class Contest1 {
+
+    public static void main(String[] args) {
+        Contest1 contest = new Contest1();
+
+        while (true) {
+//            int n = AlgorithmUtils.systemInNumberInt();
+//            int[] nums = AlgorithmUtils.systemInArray();
+            List<Integer> list = AlgorithmUtils.systemInList();
+
+//            int res = contest.solution(n, nums, list);
+//            System.out.println(res);
+        }
+
+    }
+
+    /**
+     * @return
+     */
+    private int solution(int n, int m) {
+//        int len = 0;
+//
+//        for (int i = 0; i < len; i++) {
+//
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//
+//        }
+
+        return 0;
+    }
+
+
+}
