6e638e0d9770bda7f1c3dce53634d95ef05a9fa4
辜思学 <gusixue@ipaychat.com>
2023-10-29 04:38:01 +0800

    feat：Leetcode

---
+++ src/main/java/leetcode/Main.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author gusixue
+ * @description
+ * @date 2023/10/29
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main mainClass = new Main();
+        mainClass.solution();
+    }
+
+    private void solution() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n);
+
+    }
+}
