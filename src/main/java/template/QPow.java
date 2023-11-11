fe97ad37c2f845edb48b2ef16a8113cb2a4d3263
辜思学 <gusixue@ipaychat.com>
2023-11-12 02:44:15 +0800

    Leetcode qPow

---
+++ src/main/java/template/QPow.java
@@ -0,0 +1,24 @@
+package template;
+
+/**
+ * @author gusixue
+ * @description 快速幂
+ * @date 2023/11/12
+ */
+public class QPow {
+
+    private long qPow(long value, long pow, long mod) {
+        long res = 1;
+        while (pow > 0) {
+            if ((pow & 1) == 1) {
+                res *= value;
+                res %= mod;
+            }
+
+            value *= value;
+            value %= mod;
+            pow >>= 1;
+        }
+        return res;
+    }
+}
