c10c705befa55f847379b6d1bd3ceaec41698c85
辜思学 <gusixue@ipaychat.com>
2023-11-26 02:29:17 +0800

    Leetcode contest 373 ready

---
+++ src/main/java/leetcode/contest/contest_373/Contest3.java
@@ -0,0 +1,32 @@
+package leetcode.contest.contest_373;
+
+import java.util.*;
+
+/**
+ */
+public class Contest3 {
+
+    public static void main(String[] args) {
+        Contest3 contest = new Contest3();
+
+        while (true) {
+//            int n = AlgorithmUtils.systemInNumberInt();
+//            int[] nums = AlgorithmUtils.systemInArray();
+//            List<Integer> list = AlgorithmUtils.systemInList();
+
+//            int res = contest.solution(n, nums, list);
+//            System.out.println(res);
+        }
+
+    }
+
+    /**
+     * @return
+     */
+    private int solution() {
+
+        return 0;
+    }
+
+
+}
