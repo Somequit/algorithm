a096bd37c83c0c903f09c4c2079f903ccab4599d
辜思学 <gusixue@ipaychat.com>
2024-01-24 10:35:09 +0800

    Leetcode MinimumCost

--- src/main/java/leetcode/brushQuestions/hard/MinimumCost.java
+++ src/main/java/leetcode/brushQuestions/hard/MinimumCost.java
@@ -1,5 +1,7 @@
 package leetcode.brushQuestions.hard;
 
+import javafx.util.Pair;
+
 import java.util.*;
 
 /**
@@ -11,7 +13,14 @@ public class MinimumCost {
 
     public long minimumCost(int[] nums, int k, int dist) {
         // 存下标 i，按照 nums[i] 升序
-        TreeSet<Integer> treeSetIndex = new TreeSet<>((o1, o2) -> nums[o1] - nums[o2]);
+        TreeSet<Pair<Integer, Integer>> treeSetIndex = new TreeSet<>((o1, o2) -> {
+            if (!o1.getValue().equals(o2.getValue())) {
+                return o1.getValue() - o2.getValue();
+
+            } else {
+                return o1.getKey() - o2.getKey();
+            }
+        });
 
         // 存下标 i，按照 nums[i] 升序
         Queue<Integer> heapIndex = new PriorityQueue<>((o1, o2) -> nums[o1] - nums[o2]);
@@ -24,16 +33,15 @@ public class MinimumCost {
         for (int i = 1; i < k; i++) {
             int index = heapIndex.poll();
 
-            treeSetIndex.add(index);
+            treeSetIndex.add(new Pair<>(index, nums[index]));
             curCost += nums[index];
         }
         long res = curCost;
         System.out.println(curCost + " : " + treeSetIndex + " : " + heapIndex);
         for (int i = dist + 2; i < nums.length; i++) {
             // 删除 nums[i-dist-1]，添加 nums[i]
-
             int indexDel = i - dist - 1;
-            if (treeSetIndex.remove(indexDel)) {
+            if (treeSetIndex.remove(new Pair<>(indexDel, nums[indexDel]))) {
                 curCost -= nums[indexDel];
             }
 
@@ -43,12 +51,13 @@ public class MinimumCost {
                 minIndex = heapIndex.poll();
             }
 
-            treeSetIndex.add(minIndex);
+            treeSetIndex.add(new Pair<>(minIndex, nums[minIndex]));
             curCost += nums[minIndex];
 
             if (treeSetIndex.size() > k - 1) {
-                int maxIndex = treeSetIndex.last();
-                treeSetIndex.remove(maxIndex);
+                int maxIndex = treeSetIndex.last().getKey();
+                treeSetIndex.remove(new Pair<>(maxIndex, nums[maxIndex]));
+                heapIndex.add(maxIndex);
                 curCost -= nums[maxIndex];
             }
             System.out.println(curCost + " : " + treeSetIndex + " : " + heapIndex);
