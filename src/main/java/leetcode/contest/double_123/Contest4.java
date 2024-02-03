2ef09a2d69b92681572a5d5b6e77e92a9eb614dd
辜思学 <gusixue@ipaychat.com>
2024-02-04 00:52:45 +0800

    Leetcode double 123

--- src/main/java/leetcode/contest/double_123/Contest4.java
+++ src/main/java/leetcode/contest/double_123/Contest4.java
@@ -22,36 +22,35 @@ public class Contest4 {
     /**
      * @return
      */
-    class Solution {
-        public int numberOfPairs(int[][] points) {
-            Arrays.sort(points, (o1, o2) -> {
-                if (o1[0] == o2[0]) {
-                    return o2[1] - o1[1];
-                } else {
-                    return o1[0] - o2[0];
+    public int numberOfPairs(int[][] points) {
+        Arrays.sort(points, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o2[1] - o1[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int n = points.length;
+
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            int maxPoint = Integer.MAX_VALUE;
+            for (int j = i - 1; j >= 0; j--) {
+                if (points[j][1] < points[i][1]) {
+                    continue;
                 }
-            });
 
-            int n = points.length;
-
-            int res = 0;
-            for (int i = 1; i < n; i++) {
-                int maxPoint = Integer.MAX_VALUE;
-                for (int j = i - 1; j >= 0; j--) {
-                    if (points[j][1] < points[i][1]) {
-                        continue;
-                    }
-
-                    if (points[j][1] < maxPoint) {
-                        res++;
-                        maxPoint = points[j][1];
-                    }
+                if (points[j][1] < maxPoint) {
+                    res++;
+                    maxPoint = points[j][1];
                 }
             }
-
-            return res;
         }
+
+        return res;
     }
+
 
 
 }
