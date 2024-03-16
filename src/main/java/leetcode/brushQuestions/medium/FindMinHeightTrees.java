1a0f257baf607e345a7a1397a24e000df00d365a
辜思学 <gusixue@ipaychat.com>
2024-03-17 04:22:04 +0800

    add Leetcode 310

--- src/main/java/leetcode/brushQuestions/medium/FindMinHeightTrees.java
+++ src/main/java/leetcode/brushQuestions/medium/FindMinHeightTrees.java
@@ -15,49 +15,48 @@ public class FindMinHeightTrees {
      * 找到每个点的度，循环将度为 1 的点删除，直到最后一批度为 1 的点就是结果
      */
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) {
+            List<Integer> res = new ArrayList<>();
+            res.add(0);
+            return res;
+        }
+
         int[] degree = new int[n];
-        Set<Integer>[] edgesSet = Stream.generate(HashSet::new).limit(n).toArray(Set[]::new);
-
-        Stream.of(edgesSet).forEach(System.out::println);
-
+        Map<Integer, Set<Integer>> edgesMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            edgesMap.put(i, new HashSet<>());
+        }
         for (int i = 0; i < edges.length; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
 
             degree[u]++;
             degree[v]++;
-            edgesSet[u].add(v);
-            edgesSet[v].add(u);
+            edgesMap.get(u).add(v);
+            edgesMap.get(v).add(u);
         }
 
+        List<Integer> temp = new ArrayList<>();
         List<Integer> res = Stream.iterate(0, a -> a = a + 1).limit(n).filter(i -> degree[i] <= 1).collect(Collectors.toList());
-        System.out.println(Arrays.toString(degree));
-        System.out.println(res);
-        while (true) {
-            List<Integer> temp = new ArrayList<>();
-            for (int i = 0; i < res.size(); i++) {
-                int u = res.get(i);
-                if (edgesSet[u].size() == 0) {
-                    continue;
-                }
-                int v = edgesSet[u].toArray(new Integer[1])[0];
+        while (res.size() != edgesMap.size()) {
+            temp = res;
+            res = new ArrayList<>();
 
-                System.out.println(u + " : " + v);
-                edgesSet[u].remove(v);
-                edgesSet[v].remove(u);
+            for (int i = 0; i < temp.size(); i++) {
+                int u = temp.get(i);
+                int v = 0;
+                for (int vTemp : edgesMap.get(u)) {
+                    v = vTemp;
+                }
+
+                edgesMap.remove(u);
+                edgesMap.get(v).remove(u);
                 degree[u]--;
                 degree[v]--;
-                System.out.println(degree[u] + " : " + degree[v]);
                 if (degree[v] == 1) {
-                    temp.add(v);
+                    res.add(v);
                 }
             }
-
-            if (temp.size() == 0) {
-                break;
-            }
-
-            res = temp;
         }
 
         return res;
