469f19b3d239d408b45215aa2fcdd6727918513a
辜思学 <gusixue@ipaychat.com>
2024-04-07 12:08:26 +0800

    feat: update Leetcode Contest 392

--- src/main/java/leetcode/contest/contest_392/Contest1.java
+++ src/main/java/leetcode/contest/contest_392/Contest1.java
@@ -51,4 +51,41 @@ public class Contest1 {
     }
 
 
+    public int longestMonotonicSubarray2(int[] nums) {
+        int res = 1;
+
+        int n = nums.length;
+
+        res = Math.max(res, longestIncreaseSubarray(nums));
+
+        reverseArr(nums);
+
+        res = Math.max(res, longestIncreaseSubarray(nums));
+
+        return res;
+
+    }
+
+    private int longestIncreaseSubarray(int[] nums) {
+        int res = 1;
+        for (int left = 0, right = 1; right < nums.length; right++) {
+            if (nums[right] > nums[right - 1]) {
+                res = Math.max(res, right - left + 1);
+
+            } else {
+                left = right;
+            }
+        }
+        return res;
+    }
+
+    private static void reverseArr(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[n - i - 1];
+            nums[n - i - 1] = temp;
+        }
+    }
+
 }
