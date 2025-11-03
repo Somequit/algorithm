7b5d9131080e823ed75a8d9357554318a94bd77b
辜思学 <gusixue@ipaychat.com>
2025-11-04 00:13:32 +0800

    feat：Add Codefroces Communication Template

---
+++ src/main/java/codefroces/MainCommunication.java
@@ -0,0 +1,97 @@
+package codefroces;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * @author gusixue
+ * @description 交互问题模板
+ * @date 2025/11/4 12:11 上午
+ */
+public class MainCommunication {
+    public static void main(String[] args) throws IOException {
+        String purpose = scanString();
+
+        if (purpose.equals("first")) {
+            int n = scanInt();
+
+            String res = solveFirst(n);
+
+            print(res);
+
+        } else {
+            String str = scanString();
+
+            int res = solveSecond(str);
+
+            print(res);
+        }
+    }
+
+    private static int solveSecond(String str) {
+        return 0;
+    }
+
+    private static String solveFirst(int n) {
+        return "";
+    }
+
+
+    static int MOD = 1_000_000_007;
+    static int INF = (int) 1e9;
+    static long fact[];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    static int scanInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    static long scanLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    static String scanString() throws IOException {
+        return nextToken();
+    }
+
+    static String nextToken() throws IOException {
+        if (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int[] scanIntArray(int size) throws IOException {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanInt();
+        }
+        return array;
+    }
+
+    static long[] scanLongArray(int size) throws IOException {
+        long array[] = new long[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanLong();
+        }
+        return array;
+    }
+
+    static void printArray(int arr[]) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (int e : arr) {
+            sb.append(e + " ");
+        }
+        bw.write(sb.toString().trim());
+        bw.newLine();
+        bw.flush();
+    }
+
+    static void print(Object o) throws IOException {
+        bw.write(o.toString());
+        bw.newLine();
+        bw.flush();
+    }
+}
