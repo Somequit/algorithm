233b90c6c486b078a825eaa5f00ba836aa8d9c80
辜思学 <gusixue@foxmail.com>
2025-12-14 14:18:36 +0800

    feat: Add Leetcode Contest 480_4

--- src/main/java/leetcode/contest/contest_480/Contest_480_4.java
+++ src/main/java/leetcode/contest/contest_480/Contest_480_4.java
@@ -19,15 +19,16 @@ public class Contest_480_4 {
         chars[n + 1] = s.charAt(n - 1);
 //        System.out.println(Arrays.toString(chars));
 
-        BIT bit = new BIT(n + 2);
         int curNum = 1;
-        bit.rangeAdd(0, 0, curNum);
+        long[] arr = new long[n + 2];
+        arr[0] = curNum;
         for (int i = 1; i < n + 2; i++) {
             if (chars[i] == chars[i - 1]) {
                 curNum++;
             }
-            bit.rangeAdd(i, i, curNum);
+            arr[i] = curNum;
         }
+        BITRange bit = new BITRange(arr);
 
         List<Integer> listRes = new ArrayList<>();
         for (int i = 0; i < queries.length; i++) {
@@ -36,20 +37,20 @@ public class Contest_480_4 {
                 int idx = queries[i][1] + 1;
                 if (chars[idx] == chars[idx - 1] && chars[idx] == chars[idx + 1]) {
                     // (idx)-1、(idx+1,)-2
-                    bit.rangeAdd(idx, idx, -1);
+                    bit.rangeAdd(idx, idx + 1, -1);
                     bit.rangeAdd(idx + 1, n + 2, -2);
 
                 } else if (chars[idx] != chars[idx - 1] && chars[idx] == chars[idx + 1]) {
                     // (idx)+1
-                    bit.rangeAdd(idx, idx, 1);
+                    bit.rangeAdd(idx, idx + 1, 1);
 
                 } else if (chars[idx] == chars[idx - 1] && chars[idx] != chars[idx + 1]) {
                     // (idx)-1
-                    bit.rangeAdd(idx, idx, -1);
+                    bit.rangeAdd(idx, idx + 1, -1);
 
                 } else {
                     // (idx)+1、(idx+1,)+2
-                    bit.rangeAdd(idx, idx, 1);
+                    bit.rangeAdd(idx, idx + 1, 1);
                     bit.rangeAdd(idx + 1, n + 2, 2);
 
                 }
@@ -60,7 +61,7 @@ public class Contest_480_4 {
                 int right = queries[i][2] + 1;
 
                 // (right) - (left)
-                listRes.add((int) (bit.pointQuery(right) - bit.pointQuery(left)));
+                listRes.add((int) (bit.queryForRangeSum(right, right + 1) - bit.queryForRangeSum(left, left + 1)));
             }
         }
 
@@ -68,46 +69,87 @@ public class Contest_480_4 {
     }
 
 
-    class BIT {
-        private int n;
-        private long[] tree;
+    class BITRange {
+        // 维护差分 d[i]=a[i]-a[i-1]，下标取到 [1, size)
+        long[] sum1;
+        // 维护 d[i] * i，下标取到 [1, size)
+        long[] sum2;
+        int size;
 
-        public BIT(int size) {
-            this.n = size;
-            this.tree = new long[n + 2];
+        public BITRange(int length) {
+            this.size = length + 2;
+            sum1 = new long[this.size];
+            sum2 = new long[this.size];
         }
 
-        // 在区间 [l, r] 上每个元素增加 val
-        public void rangeAdd(int l, int r, long val) {
-            if (l < 1) l = 1;
-            if (r > n) r = n;
-            if (l > r) return;
+        public BITRange(long[] arr) {
+            this.size = arr.length + 2;
+            sum1 = new long[this.size];
+            sum2 = new long[this.size];
 
-            // 差分思想：在l处加val，在r+1处减val
-            add(l, val);
-            add(r + 1, -val);
-        }
-
-        // 单点更新（内部方法）
-        private void add(int index, long val) {
-            while (index <= n + 1) {
-                tree[index] += val;
-                index += lowbit(index);
+            for (int i = 0; i < arr.length; i++) {
+                rangeAdd(i, i + 1, arr[i]);
             }
-        }
-
-        // 单点查询：获取位置 index 的值
-        public long pointQuery(int index) {
-            long sum = 0;
-            while (index > 0) {
-                sum += tree[index];
-                index -= lowbit(index);
-            }
-            return sum;
         }
 
         private int lowbit(int x) {
             return x & (-x);
         }
+
+        /**
+         * 区间更新
+         * 下标 [left, right) 增加 value
+         */
+        public void rangeAdd(int left, int right, long value) {
+            // 下标 left 的 d[i] 增加 value
+            add(sum1, left, value);
+            // 下标 right 的 d[i] 增加 -value
+            add(sum1, right, -value);
+
+            // 下标 left 的 d[i]*i 增加 value*left
+            add(sum2, left, value * left);
+            // 下标 right 的 d[i]*i 增加 -value*right
+            add(sum2, right, -value * right);
+        }
+
+        private void add(long[] sum, int index, long value) {
+            for (index++; index < this.size; index += lowbit(index)) {
+                sum[index] += value;
+            }
+        }
+
+        /**
+         * 单点求和
+         */
+        public long queryForPointSum(int index) {
+            long res = 0;
+            for (index++; index > 0; index -= lowbit(index)) {
+                res += sum1[index];
+            }
+            return res;
+        }
+
+        /**
+         * 区间求和，[left, right)
+         */
+        public long queryForRangeSum(int left, int right) {
+            return queryForPrefixSum(right) - queryForPrefixSum(left);
+        }
+
+        /**
+         * 前缀区间求和 [0, index)
+         */
+        private long queryForPrefixSum(int index) {
+            return queryForSum(sum1, index) * index - queryForSum(sum2, index);
+        }
+
+        private long queryForSum(long[] sum, int index) {
+            long res = 0;
+            for (; index > 0; index -= lowbit(index)) {
+                res += sum[index];
+            }
+            return res;
+        }
+
     }
 }
